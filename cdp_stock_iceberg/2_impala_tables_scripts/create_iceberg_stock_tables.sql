1583dd671ed189bf48769f08b6a0cf3ee3279dc2
Luiz Carrossoni <carrossoni@gmail.com>
2022-03-04 07:58:00 -0600

    Update create_iceberg_stock_tables.sql
    Updated script

--- cdp_stock_iceberg/2_impala_tables_scripts/create_iceberg_stock_tables.sql
+++ cdp_stock_iceberg/2_impala_tables_scripts/create_iceberg_stock_tables.sql
@@ -1,10 +1,5 @@
 CREATE DATABASE stocks;
 
-CREATE TABLE IF NOT EXISTS stocks.stock_intraday_1min_temp 
-(interv STRING,output_size STRING,time_zone STRING,open DECIMAL(8,4),high DECIMAL(8,4),low DECIMAL(8,4),close DECIMAL(8,4),volume BIGINT )
-PARTITIONED BY (ticker STRING,last_refreshed string,refreshed_at string)
-STORED AS iceberg;
-
 CREATE TABLE IF NOT EXISTS stocks.stock_intraday_1min
 (interv STRING,output_size STRING,time_zone STRING,open DECIMAL(8,4),high DECIMAL(8,4),low DECIMAL(8,4),close DECIMAL(8,4),volume BIGINT )
 PARTITIONED BY (ticker STRING,last_refreshed string,refreshed_at string)
